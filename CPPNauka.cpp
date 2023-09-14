155fc647b3a84f93a907f06685d883d84bffbda0
Alpha 2 <8784iirv5@gmail.com>
2023-09-14 13:59:07 +0200

    Dodaj pliki projektów.

---
+++ CPPNauka.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int dodawanie(int liczba1, int liczba2) 
+{
+    int wynik = liczba1 + liczba2;
+
+    return wynik;
+}
+
+int main()
+{
+    cout << "Wynik:" << " " << vw(2, 4);
+}
