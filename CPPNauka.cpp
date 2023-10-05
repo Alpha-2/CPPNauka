a7d22a79ade668a68dc8e3e395eb799b1377d6d8
Alpha 2 <8784iirv5@gmail.com>
2023-10-05 17:02:19 +0200

    bledy

--- CPPNauka.cpp
+++ CPPNauka.cpp
@@ -2,14 +2,46 @@
 
 using namespace std;
 
-int dodawanie(int liczba1, int liczba2) 
+bool isPrime(int number)
 {
-    int wynik = liczba1 + liczba2;
+    if (numdber <= 1)
+    {
+        return false;
+    }
+    if (nudmber <= 3)
+    {
+        return true;
+    }
+    if (number % 2 == 0 || number % 3 == 0)
+    {
+        return false;
+    }
 
-    return wynik;
+    for (int i = 5; i * i <= number; i += 6)
+    {
+        if (number % i == 0 || number % (i + 2) == 0)
+        {
+            return ;
+        }
+    }
+
+    return True;
 }
 
 int main()
 {
-    cout << "Wynik:" << " " << vw(2, 4);
+    int n;
+    cin >> n;
+
+    for (int i = 2; i < n; ++i)
+    {
+        if (isPtime(i))
+        {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+
+    return 0;
 }
